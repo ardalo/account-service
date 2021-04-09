e89c8619ce764b89c8ea9abd06303c85764663dd
Torsten Blasche <torsten.blasche@obi.de>
2021-04-10 00:35:46 +0200

    Added CI/CD pipeline

--- src/main/java/com/ardalo/digitalplatform/account/AccountServiceApplication.java
+++ src/main/java/com/ardalo/digitalplatform/account/AccountServiceApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccountServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(FrontpageServiceApplication.class, args);
+    SpringApplication.run(AccountServiceApplication.class, args);
   }
 }
--- src/test/groovy/com/ardalo/digitalplatform/account/login/LoginControllerIT.groovy
+++ src/test/groovy/com/ardalo/digitalplatform/account/login/LoginControllerIT.groovy
@@ -1,4 +1,4 @@
-package com.ardalo.digitalplatform.account.frontpage
+package com.ardalo.digitalplatform.account.login
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
